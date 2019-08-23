872c5c4e1717226ad5b1484cfc00c2a3d2d21318
Ubuntu <ubuntu@ip-172-31-30-166.us-east-2.compute.internal>
2019-08-23 16:06:29 +0000

    registration form added

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
-from accounts.forms import UserLoginForm
+from accounts.forms import UserLoginForm, UserRegistrationForm
 
 def index(request):
     """Return the index.html file"""
@@ -22,8 +22,6 @@ def login(request):
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
                                     password=request.POST['password'])
-            
-
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in!")
@@ -32,3 +30,9 @@ def login(request):
     else:
         login_form = UserLoginForm()
     return render(request, 'login.html', {'login_form': login_form})
+    
+def registration(request):
+    """Render registration page"""
+    registration_form = UserRegistrationForm()
+    return render(request, 'registration.html', 
+    {"registration_form": registration_form})
